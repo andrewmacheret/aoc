65ce2c5f7b24fea09764476e389d65def0e98dde
Andrew Macheret <amacheret@atlassian.com>
2018-12-24 11:51:21 -0800

    day23 - figured out part 2 using z3

--- 2018/python/day23/main.py
+++ 2018/python/day23/main.py
@@ -1,19 +1,7 @@
 import re
-import networkx as nx
-import numpy as np
-from collections import OrderedDict, defaultdict, deque, Counter
-from pprint import pprint
-from copy import copy, deepcopy
-from heapq import heappush, heappop, heappushpop, heapify, heapreplace, merge, nlargest, nsmallest
-import sys
-import io
-
-sys.setrecursionlimit(100000)
+from z3 import Optimize, Int, If  # pip install z3-solver
 
 class Solution:
-  def __init__(self):
-    pass
-
   def load(self, filename):
     self.filename = filename
     with open(filename) as f: self.lines = f.read().splitlines()
@@ -28,18 +16,43 @@ class Solution:
 
     return self
 
-  def distance(self, (x1, y1, z1), (x2, y2, z2)):
-    return abs(x1-x2) + abs(y1-y2) + abs(z1-z2)
+  def distance(self, (x1, y1, z1), (x2, y2, z2), abs_func=abs):
+    return abs_func(x1-x2) + abs_func(y1-y2) + abs_func(z1-z2)
+
+  def maximize_distance(self):
+    o = Optimize()
+
+    z3_abs = lambda i: If(i >= 0, i, -i)
+    z3_in_ranges = [ Int('in_range_of_bot_' + str(i)) for i in xrange(len(self.bots)) ]
+    z3_xyz = (Int('x'), Int('y'), Int('z'))
+    z3_sum = Int('sum')
+    z3_dist = Int('dist')
+
+    for i, bot in enumerate(self.bots):
+      o.add(z3_in_ranges[i] == If(self.distance(z3_xyz, bot['pos'], z3_abs) <= bot['radius'], 1, 0))
+
+    o.add(z3_sum == sum(z3_in_ranges))
+
+    o.add(z3_dist == self.distance(z3_xyz, (0,0,0), z3_abs))
+
+    h1 = o.maximize(z3_sum)
+    h2 = o.minimize(z3_dist)
+    o.check()
+    # o.lower(h1), o.upper(h1)
+
+    return o.lower(h2), o.upper(h2)
+    # o.model()[Int('x')], o.model()[Int('y')], o.model()[Int('z')]
 
   def solve(self):
     max_radius = max(bot['radius'] for bot in self.bots)
     best_bot = next(bot for bot in self.bots if bot['radius'] == max_radius)
     in_range = sum(1 for bot in self.bots if self.distance(bot['pos'], best_bot['pos']) <= max_radius)
+    max_dist = self.maximize_distance()
 
     return {
       'part1': in_range,
+      'part2': max_dist
     }
-
 
 print(Solution().load('input-test.txt').solve())
 print(Solution().load('input-test2.txt').solve())
