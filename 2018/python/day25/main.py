ab9e46a5d61807ad477d5b2071b041a8dbfacd73
Andrew Macheret <amacheret@atlassian.com>
2018-12-26 15:44:56 -0800

    day 25 - yet even faster by precomputing all the directions instead of presorting

--- 2018/python/day25/main.py
+++ 2018/python/day25/main.py
@@ -6,22 +6,34 @@ class Solution:
   def load(self, filename):
     self.filename = filename
     with open(filename) as f: lines = f.read().splitlines()
-    self.points = sorted(tuple(int(i) for i in line.strip().split(',')) for line in lines)
+    self.points = [tuple(int(i) for i in line.strip().split(',')) for line in lines]
     return self
 
-  def count_clusters(self):
+  def count_clusters(self, dist=3):
     n = len(self.points)
     points = self.points
-    _abs = abs
     uf = UnionFind(n)
+
+    # compute all (dx,dy,dz,dt) such that abs(dx) + abs(dy) + abs(dz) + abs(dt) <= 3
+    # and don't include (0,0,0,0)
+    DIRS = [(dx,dy,dz,dt) for dx in xrange(-dist, dist+1) \
+                          for dy in xrange(-dist+abs(dx), dist+1-abs(dx)) \
+                          for dz in xrange(-dist+abs(dx)+abs(dy), dist+1-abs(dx)-abs(dy)) \
+                          for dt in xrange(-dist+abs(dx)+abs(dy)+abs(dz), dist+1-abs(dx)-abs(dy)-abs(dz)) \
+                          if (dx,dy,dz,dt) != (0,0,0,0)]
+
+    # generate a dict of each point to its index
+    point_to_index = {p: i for i, p in enumerate(points)}
+
+    # for each connected points, perform a union find between the indices of the two points
     for i in xrange(n):
-      x1,y1,z1,t1 = points[i]
-      for j in xrange(i+1, n):
-        x2,y2,z2,t2 = points[j]
-        x = x2-x1
-        if x + _abs(y2-y1) + _abs(z2-z1) + _abs(t2-t1) <= 3:
-          uf.union(i, j)
-        elif x >= 3 and (x > 3 or (y2-y1 >= 0 and (y2-y1 > 0 or (z2-z1 >= 0 and (z2-z1 > 0 or t2-t1 >= 0))))): break
+      x,y,z,t = points[i]
+      for dx,dy,dz,dt in DIRS:
+        p2 = (x+dx, y+dy, z+dz, t+dt)
+        if p2 in point_to_index:
+          uf.union(i, point_to_index[p2])
+
+    # return the number of disjoint sets we found
     self.result = uf.count()
 
   def solve(self):
