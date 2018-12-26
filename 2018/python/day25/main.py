d71e2188bdd1d43848797d8eed559aea3fbb04a9
Andrew Macheret <amacheret@atlassian.com>
2018-12-26 14:05:02 -0800

    day 25 - replace custom search with a standard UnionFind library

--- 2018/python/day25/main.py
+++ 2018/python/day25/main.py
@@ -1,54 +1,36 @@
 from collections import defaultdict
 from timeit import timeit
+from python_algorithms.basic.union_find import UF as UnionFind
 
 class Solution:
   def load(self, filename):
     self.filename = filename
     with open(filename) as f: lines = f.read().splitlines()
-    self.points = sorted([int(i) for i in line.strip().split(',')] for line in lines)
+    self.points = sorted(tuple(int(i) for i in line.strip().split(',')) for line in lines)
     return self
 
-  def prepare_graph(self):
-    self.n = n = len(self.points)
-    graph = defaultdict(list)
+  def count_clusters(self):
+    n = len(self.points)
     points = self.points
     _abs = abs
+    uf = UnionFind(n)
     for i in xrange(n):
       x1,y1,z1,t1 = points[i]
       for j in xrange(i+1, n):
         x2,y2,z2,t2 = points[j]
         x = x2-x1
         if x + _abs(y2-y1) + _abs(z2-z1) + _abs(t2-t1) <= 3:
-          graph[i].append(j)
-          graph[j].append(i)
+          uf.union(i, j)
         elif x >= 3 and (x > 3 or (y2-y1 >= 0 and (y2-y1 > 0 or (z2-z1 >= 0 and (z2-z1 > 0 or t2-t1 >= 0))))): break
-    self.graph = graph
-
-  def count_clusters(self):
-    count = 0
-    visited = set()
-    n = self.n
-    graph = self.graph
-    for i in xrange(n):
-      if i not in visited:
-        count += 1
-        q = [i]
-        while q:
-          j = q.pop()
-          visited.add(j)
-          for k in graph[j]:
-            if k not in visited:
-              q.append(k)
-    self.result = count
+    self.result = uf.count()
 
   def solve(self):
-    time1 = timeit(stmt=self.prepare_graph, number=1)
-    time2 = timeit(stmt=self.count_clusters, number=1)
+    time = timeit(stmt=self.count_clusters, number=1)
 
     return [
       {'filename': self.filename},
       {'part1': self.result},
-      {'time': time1+time2},
+      {'time': time},
     ]
 
 print(Solution().load('input-test.txt').solve())
