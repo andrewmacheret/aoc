d5b22f65ae10408bd5d159420156d2bd9de6072c
Andrew Macheret <andrew.macheret@gmail.com>
2019-12-01 23:17:44 -0800

    Clean up eval, add sympy

--- 2019/python/day02/main.py
+++ 2019/python/day02/main.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import operator
+from sympy import Symbol, Eq, S, solve
 
 class Solution:
   def __init__(self):
@@ -23,13 +24,10 @@ class Solution:
       elif opcode == 99:
         return
 
-  def run_computer_with_numbers(self):
-    return self.run_computer(0, operator.add, operator.mul)
-
-  def run_with_grammar(self, noun, verb):
+  def run_with_grammar(self, noun, verb, skip=0, add=operator.add, mul=operator.mul):
     self.memory[1] = noun
     self.memory[2] = verb
-    self.run_computer_with_numbers()
+    self.run_computer(skip, add, mul)
     return self.memory[0]
 
   def part1(self):
@@ -44,20 +42,31 @@ class Solution:
           return noun, verb
 
   def part2_eval(self, goal):
-    self.memory[1] = '{0}'
-    self.memory[2] = '{1}'
     fake_add = lambda a, b: '({}+{})'.format(a, b)
     fake_mul = lambda a, b: '({}*{})'.format(a, b)
-    self.run_computer(4, fake_add, fake_mul)
+    self.run_with_grammar('noun', 'verb', skip=4, add=fake_add, mul=fake_mul)
 
     expression = self.memory[0]
     for noun in range(1, 100):
       for verb in range(1, 100):
-        if eval(expression.format(noun, verb)) == goal:
+        if eval(expression) == goal:
           return noun, verb
+
+  def part2_solve_imperfect(self, goal, override_noun=None, override_verb=None):
+    fake_add = lambda a, b: '({}+{})'.format(a, b)
+    fake_mul = lambda a, b: '({}*{})'.format(a, b)
+    self.run_with_grammar('noun', 'verb', skip=4, add=fake_add, mul=fake_mul)
+    expression = self.memory[0]
+    noun = override_noun or Symbol('noun')
+    verb = override_verb or Symbol('verb')
+    return solve(Eq(eval(expression), goal), noun, verb)
 
 print('Part 1', Solution().load('input.txt').part1())
 
 print('Part 2', Solution().load('input.txt').part2(19690720))
 
 print('Part 2, with eval', Solution().load('input.txt').part2_eval(19690720))
+
+print('Part 2, with sympy', Solution().load('input.txt').part2_solve_imperfect(19690720))
+# above outputs `noun = 761401/8100 - verb/202500` which is effectively `noun = 94`
+print('Part 2, with sympy', Solution().load('input.txt').part2_solve_imperfect(19690720, override_noun=94))
