928150b9e579e920d68fd3880ad7f346df91a5cd
Andrew Macheret <andrew.macheret@gmail.com>
2019-12-04 00:27:49 -0800

    Day 4 improvements

--- 2019/python/day04/main.py
+++ 2019/python/day04/main.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 from collections import Counter
+from itertools import islice
 import os
 
 os.sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) 
 from day01.main import load, test
 
-def is_not_decreasing(indexable):
-  return all(a <= b for a, b in zip(indexable, indexable[1:]))
+def is_sorted(iterable):
+  return all(a <= b for a, b in zip(iterable, islice(iterable, 1, None)))
 
-def has_double(indexable):
-  return any(a == b for a, b in zip(indexable, indexable[1:]))
+def has_at_least_n(iterable, n):
+  return any(i >= n for i in Counter(iterable).values())
 
-def has_exactly_double(indexable):
-  return 2 in Counter(indexable).values()
+def has_exactly_n(iterable, n):
+  return n in Counter(iterable).values()
 
 def part1(filename):
   start, end = map(int, load(filename, script=__file__)[0].split('-', 2))
-  return sum(1 for s in map(str, range(start, end + 1)) if is_not_decreasing(s) and has_double(s))
+  return sum(1 for s in map(str, range(start, end + 1)) if is_sorted(s) and has_at_least_n(s, 2))
 
 def part2(filename):
   start, end = map(int, load(filename, script=__file__)[0].split('-', 2))
-  return sum(1 for s in map(str, range(start, end + 1)) if is_not_decreasing(s) and has_exactly_double(s))
+  return sum(1 for s in map(str, range(start, end + 1)) if is_sorted(s) and has_exactly_n(s, 2))
 
 if __name__== "__main__":
   test(1, part1('input-test-1.txt'))
