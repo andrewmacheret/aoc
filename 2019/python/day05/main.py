80e6e56e996b688b070169c47ea973344d9c5903
Andrew Macheret <andrew.macheret@gmail.com>
2019-12-05 00:34:35 -0800

    Clean up day 5 a bit

--- 2019/python/day05/main.py
+++ 2019/python/day05/main.py
@@ -8,50 +8,44 @@ from day01.main import test
 from day02.main import load_memory
 
 def run_computer(memory, input):
-  parse_modes = lambda modes, n: list(map(int, str(modes).zfill(n)))[:n]
-  
   output = []
   pos = 0
 
   def grab_vals(modes, n, positionals=[]):
     nonlocal pos
-    output = []
     for i in range(1, n+1):
       val = memory[pos]
       mode = modes % 10
-      output.append(val if mode or i in positionals else memory[val])
+      yield val if mode or i in positionals else memory[val]
       modes //= 10
       pos += 1
-    # print('params', output)
-    return output if len(output) != 1 else output[0]
 
   while pos < len(memory):
     modes, opcode = divmod(memory[pos], 100)
     pos += 1
-    # print('opcode={} modes={}'.format(opcode, modes))
     if opcode == 1:
-      a, b, c = grab_vals(modes, 3, [3])
+      a, b, c = list(grab_vals(modes, 3, [3]))
       memory[c] = a + b
     elif opcode == 2:
-      a, b, c = grab_vals(modes, 3, [3])
+      a, b, c = list(grab_vals(modes, 3, [3]))
       memory[c] = a * b
     elif opcode == 3:
-      a = grab_vals(modes, 1, [1])
+      a = list(grab_vals(modes, 1, [1]))[0]
       memory[a] = input.popleft()
     elif opcode == 4:
-      a = grab_vals(modes, 1, [])
+      a = list(grab_vals(modes, 1))[0]
       output.append(a)
     elif opcode == 5:
-      a, b = grab_vals(modes, 2, [])
+      a, b = list(grab_vals(modes, 2))
       if a != 0: pos = b
     elif opcode == 6:
-      a, b = grab_vals(modes, 2, [])
+      a, b = list(grab_vals(modes, 2))
       if a == 0: pos = b
     elif opcode == 7:
-      a, b, c = grab_vals(modes, 3, [3])
+      a, b, c = list(grab_vals(modes, 3, [3]))
       memory[c] = int(a < b)
     elif opcode == 8:
-      a, b, c = grab_vals(modes, 3, [3])
+      a, b, c = list(grab_vals(modes, 3, [3]))
       memory[c] = int(a == b)
     elif opcode == 99:
       return output
