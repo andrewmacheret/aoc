ef76a9ced375408a92969bb326ff73354305c519
Andrew Macheret <amacheret@atlassian.com>
2020-12-13 13:53:35 -0800

    Update template

--- templates/python-template.py
+++ templates/python-template.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
-import re
-import networkx as nx
-import numpy as np
-from collections import *
-from itertools import *
-from pprint import pprint
-from copy import copy, deepcopy
-from heapq import *
-from operator import *
-from functools import *
-from math import *
-import sys
-import io
-import os
-
-sys.setrecursionlimit(100000)
-
 from day01.main import load, test
+# import re
+# import networkx as nx
+# import numpy as np
+# from collections import *
+# from itertools import *
+# from pprint import pprint
+# from copy import copy, deepcopy
+# from heapq import *
+# from operator import *
+# from functools import *
+# from math import *
+# import sys
+# import io
+# import os
+# sys.setrecursionlimit(100000)
+
 
 def load_custom(filename, script=__file__):
-  return [line.split(',') for line in load(filename, script=script)]
+    return [line.split(',') for line in load(filename, script=script)]
+
 
 def part1(filename):
-  data = load_custom(filename)
-  return None
+    data = load_custom(filename)
+    return None
 
 # def part2(filename):
 #   data = load_custom(filename)
 #   return None
 
-if __name__== "__main__":
-  test(None, part1('input-test-1.txt'))
-  # test(None, part1('input.txt'))
-  # 
-  # test(None, part2('input-test-1.txt'))
-  # test(None, part2('input.txt'))
+
+if __name__ == "__main__":
+    test(None, part1('input-test-1.txt'))
+    # test(None, part1('input.txt'))
+    #
+    # test(None, part2('input-test-1.txt'))
+    # test(None, part2('input.txt'))
