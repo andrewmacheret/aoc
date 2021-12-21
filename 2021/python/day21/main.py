93b7eeaed3ef8c5c2637d68808a5d04061c8b49d
Andrew Macheret <amacheret@atlassian.com>
2021-12-21 14:27:05 -0800

    Combine part 1 and part 2

--- 2021/python/day21/main.py
+++ 2021/python/day21/main.py
@@ -6,40 +6,41 @@ from itertools import cycle, product
 from common.util import load, test, change_dir, parse_nums
 
 
-def part1(players, goal=1000, dice_sides=100):
-  dice = cycle(range(1, dice_sides+1))
-  scores = [0, 0]
-  for round, turn in enumerate(cycle((0, 1)), 1):
-    roll = next(dice) + next(dice) + next(dice)
-    players[turn] = (players[turn] + roll) % 10 or 10
-    scores[turn] += players[turn]
-    if scores[turn] >= goal:
-      return (round * 3) * scores[1 - turn]
+def deterministic_dice(sides):
+  gen = cycle(range(1, sides+1))
+  return lambda: {next(gen) + next(gen) + next(gen): 1}
 
 
-def part2(players, goal=21, dice_sides=3):
-  roll_counts = Counter(i+j+k for i, j, k in
-                        product(range(1, dice_sides+1), repeat=3))
+def quantum_dice(sides):
+  counter = Counter(i+j+k for i, j, k in product(range(1, sides+1), repeat=3))
+  return lambda: counter
 
+
+def dirac_dice(players, roll_counts, goal):
   dp = Counter([(*players, 0, 0)])
   totals = [0, 0]
-  for turn in cycle((0, 1)):
+  first_to_win = None
+  for round, turn in enumerate(cycle((0, 1)), 1):
     dp_next = Counter()
     for (player1, player2, score1, score2), universes in dp.items():
-      if score2 < goal:
-        for roll, roll_count in roll_counts.items():
-          next_player1 = (player1 + roll) % 10 or 10
-          next_state = (player2, next_player1, score2, score1 + next_player1)
+      for roll, roll_count in roll_counts().items():
+        next_player1 = (player1 + roll) % 10 or 10
+        next_score1 = score1 + next_player1
+        if next_score1 < goal:
+          next_state = (player2, next_player1, score2, next_score1)
           dp_next[next_state] += universes * roll_count
-      else:
-        totals[turn] += universes
+        else:
+          if not first_to_win:
+            first_to_win = (round * 3) * score2
+          totals[turn] += universes * roll_count
     if not (dp := dp_next):
-      return max(totals)
+      return first_to_win, max(totals)
 
 
 def solve(part, file):
   players = [parse_nums(line)[-1] for line in load(file)]
-  return (part1, part2)[part - 1](players)
+  args = (deterministic_dice(100), 1000) if part == 1 else (quantum_dice(3), 21)
+  return dirac_dice(players, *args)[part - 1]
 
 
 ### THE REST IS TESTS ###
@@ -50,4 +51,5 @@ if __name__ == "__main__":
   test(739785, solve(part=1, file='input-test-1'))
   test(903630, solve(part=1, file='input-real'))
 
+  test(444356092776315, solve(part=2, file='input-test-1'))
   test(303121579983974, solve(part=2, file='input-real'))
