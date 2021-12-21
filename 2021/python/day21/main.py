038e71a203ee6f9f46b6637b8058ab437f0e030b
Andrew Macheret <amacheret@atlassian.com>
2021-12-21 13:33:14 -0800

    Use Counter

--- 2021/python/day21/main.py
+++ 2021/python/day21/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
+from collections import Counter
 from itertools import cycle, product
 
 from common.util import load, test, change_dir, parse_nums
@@ -22,21 +22,19 @@ def part1(players, goal=1000, die_sides=100):
 
 
 def part2(players, goal=21, die_sides=3):
-  roll_counts = defaultdict(int)
-  for i, j, k in product(range(1, die_sides+1), repeat=3):
-    roll_counts[i+j+k] += 1
+  roll_counts = Counter(i+j+k for i, j, k in
+                        product(range(1, die_sides+1), repeat=3))
 
-  dp = defaultdict(int, {(*players, 0, 0): 1})
+  dp = Counter([(*players, 0, 0)])
   totals = [0, 0]
   for turn in cycle((0, 1)):
-    dp_next = defaultdict(int)
+    dp_next = Counter()
     for (player1, player2, score1, score2), universes in dp.items():
       if score2 < goal:
         for roll, roll_count in roll_counts.items():
-          player1_next = mod_up(player1 + roll)
-          score1_next = score1 + player1_next
-          dp_next[player2, player1_next, score2, score1_next] \
-              += universes * roll_count
+          next_player1 = mod_up(player1 + roll)
+          next_state = (player2, next_player1, score2, score1 + next_player1)
+          dp_next[next_state] += universes * roll_count
       else:
         totals[turn] += universes
     if not (dp := dp_next):
