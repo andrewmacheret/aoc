c1732436ca3eb916147511a10f46048b1f796bd5
Andrew Macheret <amacheret@atlassian.com>
2024-12-15 15:53:59 -0800

    2024 day 15

--- 2024/python/day15/main.py
+++ 2024/python/day15/main.py
@@ -1,134 +1,72 @@
 #!/usr/bin/env python3
 
-# import networkx as nx
-# import numpy as np
-# from copy import copy, deepcopy
-# import sys
-# sys.setrecursionlimit(100000)
-
 from common.util import *
 
 dirs = {
-  '<': (-1, 0),
-  '>': (1, 0),
-  '^': (0, -1),
-  'v': (0, 1),
+  '<': LEFT,
+  '>': RIGHT,
+  '^': UP,
+  'v': DOWN,
 }
 
-def solve(part, file):
-  # data = load_ints(file)
-  # data = load_custom(file)
-  # data = load_blocks(file)
-  # data = load_csv(file)
-  g, m = load_blocks(file)
-  grid = parse_grid(g)
-  m = list(''.join(m))
+expansions = {
+  '@': '@.',
+  '#': '##',
+  '.': '..',
+  'O': '[]',
+}
 
-  grid2 = []
-  for y in range(len(grid)):
-    grid2.append([])
-    for x in range(len(grid[y])):
-      if grid[y][x] == '@':
-        grid2[y].append('@')
-        grid2[y].append('.')
-      elif grid[y][x] == '#':
-        grid2[y].append('#')
-        grid2[y].append('#')
-      elif grid[y][x] == '.':
-        grid2[y].append('.')
-        grid2[y].append('.')
-      elif grid[y][x] == 'O':
-        grid2[y].append('[')
-        grid2[y].append(']')
-  grid = grid2
-
-  sx, sy = 0, 0
-  for y in range(len(grid)):
-    for x in range(len(grid[y])):
-      if grid[y][x] == '@':
-        sx, sy = x, y
-        break
-
-  # for mov in m:
-  #   dx, dy = dirs[mov]
-  #   x1 = sx + dx
-  #   y1 = sy + dy
-  #   if grid[y1][x1] == '#':
-  #     continue
-  #   if grid[y1][x1] == '.':
-  #     grid[sy][sx] = '.'
-  #     grid[y1][x1] = '@'
-  #     sx, sy = x1, y1
-  #     continue
-  #   for i in count(1):
-  #     x2 = sx + dx * i
-  #     y2 = sy + dy * i
-  #     if grid[y2][x2] == '#':
-  #       break
-  #     if grid[y2][x2] == '.':
-  #       grid[y2][x2] = 'O'
-  #       grid[y1][x1] = '@'
-  #       grid[sy][sx] = '.'
-  #       sx, sy = x1, y1
-  #       break
-
-  for mov in m:
-    # print(mov)
-    # print(draw_grid(grid))
-    dx, dy = dirs[mov]
-    x1 = sx + dx
-    y1 = sy + dy
-    if grid[y1][x1] == '#':
-      continue
-    if grid[y1][x1] == '.':
-      grid[sy][sx] = '.'
-      grid[y1][x1] = '@'
-      sx, sy = x1, y1
-      continue
+def push_boxes(grid, sx, sy, dx, dy):
+  # build a stack of the moves, starting with the player
+  stack = [{(sx,sy)}]
+  while True:
+    # any walls, do nothing
+    if any(grid[y+dy][x+dx] == '#' for x,y in stack[-1]):
+      return sx, sy
     
-    q = [{(sx,sy)}]
-    doit = False
-    for i in count():
-      print(q)
-      if any(grid[y+dy][x+dx] == '#' for x,y in q[-1]):
-        break
-      if all(grid[y+dy][x+dx] == '.' for x,y in q[-1]):
-        doit = True
-        break
-      new = set()
-      if dy == 0:
-        for x,y in q[-1]:
-          if grid[y+dy][x+dx] in '[]':
-            new.add((x+dx, y+dy))
-      else:
-        for x,y in q[-1]:
-          if grid[y+dy][x+dx] == '[':
-            new.add((x+dx, y+dy))
-            new.add((x+dx+1, y+dy))
-          elif grid[y+dy][x+dx] == ']':
-            new.add((x+dx, y+dy))
-            new.add((x+dx-1, y+dy))
-      q.append(new)
-    if doit:
-      while q:
-        for x,y in q.pop():
+    # all empty, perform the move
+    if all(grid[y+dy][x+dx] == '.' for x,y in stack[-1]):
+      # move all boxes in the stack, one row at a time
+      while stack:
+        for x,y in stack.pop():
           grid[y+dy][x+dx] = grid[y][x]
           grid[y][x] = '.'
-      # grid[sy][sx] = '.'
-      sx, sy = sx+dx, sy+dy
+      return sx+dx, sy+dy
+    
+    # build a new set of boxes in the stack, based on the last row
+    new = set()
+    for x,y in stack[-1]:
+      # always add the next cell if it's a box
+      if grid[y+dy][x+dx] in '[]O':
+        new.add((x+dx, y+dy))
+      # vertical only...
+      if dy: 
+        # add right cell if it's the left side of a box
+        if grid[y+dy][x+dx] == '[':
+          new.add((x+dx+1, y+dy))
+        # add left cell if it's the right side of a box
+        elif grid[y+dy][x+dx] == ']':
+          new.add((x+dx-1, y+dy))
+    stack.append(new)
 
-  total = 0
-  for y, row in enumerate(grid):
-    for x, ch in enumerate(row):
-      if ch == '[':
-        total += y*100 + x
-  # print(draw_grid(grid))
-      
-  
+def solve(part, file):
+  grid, moves = load_blocks(file)
+  grid = parse_grid(grid)
+  moves = ''.join(moves)
 
+  # expand the grid if part 2
+  if part:
+    grid = [[c for ch in row for c in expansions[ch]] for row in grid]
 
-  #print(data)
-  return total
+  # find the player
+  sx, sy = next((x,y) for y, row in enumerate(grid) for x, ch in enumerate(row) if ch == '@')
+
+  # perform all the moves
+  for move in moves:
+    sx, sy = push_boxes(grid, sx, sy, *dirs[move])
+
+  # sum based on the left side of all boxes
+  return sum(y*100 + x for y, row in enumerate(grid) for x, ch in enumerate(row) if ch in '[O')
 
 
 ### THE REST IS TESTS ###
@@ -136,8 +74,8 @@ def solve(part, file):
 if __name__ == "__main__":
   change_dir(__file__)
 
-  # test(None, solve(part=0, file='input-test-1'))
-  # test(None, solve(part=0, file='input-real'))
+  test(10092, solve(part=0, file='input-test-1'))
+  test(1429911, solve(part=0, file='input-real'))
 
-  test(None, solve(part=1, file='input-test-1'))
-  test(None, solve(part=1, file='input-real'))
+  test(9021, solve(part=1, file='input-test-1'))
+  test(1453087, solve(part=1, file='input-real'))
